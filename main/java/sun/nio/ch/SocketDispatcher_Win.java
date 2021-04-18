02b1c9739a80f8e69dfd977dc72be363ec87b1de
503923814 <503923814@qq.com>
2021-04-18 22:48:24 +0800

    添加文件

---
+++ main/java/sun/nio/ch/SocketDispatcher_Win.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2000, 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package sun.nio.ch;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+
+/**
+ * Allows different platforms to call different native methods
+ * for read and write operations.
+ */
+
+class SocketDispatcher_Win extends NativeDispatcher
+{
+
+    static {
+        IOUtil.load();
+    }
+
+    int read(FileDescriptor fd, long address, int len) throws IOException {
+        return read0(fd, address, len);
+    }
+
+    long readv(FileDescriptor fd, long address, int len) throws IOException {
+        return readv0(fd, address, len);
+    }
+
+    int write(FileDescriptor fd, long address, int len) throws IOException {
+        return write0(fd, address, len);
+    }
+
+    long writev(FileDescriptor fd, long address, int len) throws IOException {
+        return writev0(fd, address, len);
+    }
+
+    void preClose(FileDescriptor fd) throws IOException {
+        preClose0(fd);
+    }
+
+    void close(FileDescriptor fd) throws IOException {
+        close0(fd);
+    }
+
+    //-- Native methods
+    static native int read0(FileDescriptor fd, long address, int len)
+        throws IOException;
+
+    static native long readv0(FileDescriptor fd, long address, int len)
+        throws IOException;
+
+    static native int write0(FileDescriptor fd, long address, int len)
+        throws IOException;
+
+    static native long writev0(FileDescriptor fd, long address, int len)
+        throws IOException;
+
+    // https://docs.microsoft.com/en-us/windows/win32/api/winsock/nf-winsock-getsockopt
+    // https://docs.microsoft.com/en-us/windows/win32/winsock/sol-socket-socket-options
+
+    // SO_LINGER: Indicates the state of the linger structure associated with a socket. If the l_onoff
+    // member of the linger structure is nonzero, a socket remains open for a specified amount
+    // of time after a closesocket function call to enable queued data to be sent. The amount
+    // of time, in seconds, to remain open is specified in the l_linger member of the linger
+    // structure. This option is only valid for reliable, connection-oriented protocols.
+
+    // If no error occurs, getsockopt returns zero.
+
+    /**
+     * if (getsockopt(fd, SOL_SOCKET, SO_LINGER, (char *)&l, &len) == 0) {
+     *       if (l.l_onoff == 0) {
+     * 	// The WSASendDisconnect function initiates termination of the connection for the socket and sends disconnect data.
+     *          WSASendDisconnect(fd, NULL);
+     *       }
+     *     }
+     */
+    static native void preClose0(FileDescriptor fd) throws IOException;
+
+    static native void close0(FileDescriptor fd) throws IOException;
+}
